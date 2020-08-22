aa5f90c202cf96c2f2076d3334c2dc68e2c8b6de
Syn-Tax <twocap06@gmail.com>
2020-08-22 16:31:56 +0100

    did some notifications

---
+++ scripts/alert.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo $1
+
+if [ "$1" == "notify-send" ]; then mpv "/home/oscar/scripts/juntos.ogg" ; fi
+if [ "$1" == "Emacs" ]; then mpv "/home/oscar/scripts/juntos.ogg" ; fi
---
+++ scripts/notes-sync.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cd /home/oscar/Notes
+git add *
+git commit -am "Automated Commit"
+git push
+git pull
