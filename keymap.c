8088e30f7c9f932575d39ba27ee1f01dd2e8cc11
Oscar Morris <twocap06@gmail.com>
2020-09-29 08:27:29 +0100

    Add files via upload

--- keymap.c
+++ keymap.c
@@ -30,13 +30,13 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* Base (qwerty)
+/* Base (dvorak)
  * ,----------------------------------,                             ,----------------------------------,
- * |   q  |   w  |   e  |   r  |   t  |                             |   y  |   u  |   i  |   o  |   p  |
+ * |   '  |   ,  |   .  |   p  |   y  |                             |   f  |   g  |   c  |   r  |   l  |
  * |------+------+------+------+------|                             |-------------+------+------+------|
- * |   a  |   s  |   d  |   f  |   g  |                             |   h  |   j  |   k  |   l  |   ;  |
+ * |   a  |   o  |   e  |   u  |   i  |                             |   d  |   h  |   t  |   n  |   s  |
  * |------+------+------+------+------|                             |------|------+------+------+------|
- * |   z  |   x  |   c  |   v  |   b  |                             |   n  |   m  |   ,  |   .  |   '  |
+ * |   ;  |   q  |   j  |   k  |   x  |                             |   b  |   m  |   w  |   v  |   z  |
  * |------+------+------+-------------,                             ,-------------+------+------+------,
  *        |  [   |   ]  |                                                         |   (  |   )  |
  *        '------+------'-------------'                             '-------------'------+------'
@@ -52,9 +52,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_BASE] = LAYOUT( \
-  KC_Q, KC_W, KC_E,    KC_R,    KC_T,                                       KC_Y, KC_U,    KC_I,    KC_O,   KC_P,    \
-  KC_A, KC_S, KC_D,    KC_F,    KC_G,                                       KC_H, KC_J,    KC_K,    KC_L,   KC_SCLN, \
-  KC_Z, KC_X, KC_C,    KC_V,    KC_B,                                       KC_N, KC_M,    KC_COMM, KC_DOT, KC_QUOT, \
+  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                                       KC_F, KC_G,    KC_C,    KC_R,   KC_L,    \
+  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                                       KC_D, KC_H,    KC_T,    KC_N,   KC_S, \
+  KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,                                       KC_B, KC_M,    KC_W,    KC_V,   KC_Z, \
               KC_LBRC, KC_RBRC,                                                   KC_LPRN, KC_RPRN,                   \
                                       ALT_ESC, KC_BSPC,   KC_SPC,  KC_ENT,                                          \
                                       KC_LSFT, RAISE  ,   KC_LGUI, LOWER,                                           \
@@ -63,11 +63,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Raise
  * ,----------------------------------,                             ,----------------------------------,
- * | F1   | F2   | F3   | F4   | F5   |                             | VOL+ |      |      |      | PgUp |
+ * | F1   | F2   | F3   | F4   | F5   |                             | VOL+ |      |      |      | right|
  * |------+------+------+------+------|                             |-------------+------+------+------|
- * | F6   | F7   | F8   | F9   | F10  |                             | left | down | up   |right | PgDn |
+ * | F6   | F7   | F8   | F9   | F10  |                             |      | left |      |      |      |
  * |------+------+------+------+------|                             |------|------+------+------+------|
- * | F11  | F12  |      |      |      |                             | VOL- |      |      |      |      |
+ * | F11  | F12  | down | up   |      |                             | VOL- |      |      |      |      |
  * |------+------+------+-------------,                             ,-------------+------+------+------,
  *        |      |      |                                                         |      |      |
  *        '------+------'-------------'                             '-------------'------+------'
@@ -83,9 +83,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_RAISE] = LAYOUT( \
-  KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                          KC_VOLU, ____,    ____,    ____,     KC_PGUP,   \
-  KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,                         KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_PGDOWN, \
-  KC_F11,KC_F12,  ____,  ____,  ____,                         KC_VOLD, ____,    ____,    ____,     ____,      \
+  KC_F1, KC_F2, KC_F3,     KC_F4, KC_F5,                          KC_VOLU, ____,    ____,    ____,  KC_RIGHT,   \
+  KC_F6, KC_F7, KC_F8,     KC_F9, KC_F10,                         ____,    KC_LEFT, ____,    ____,  ____, \
+  KC_F11,KC_F12,  KC_DOWN, KC_UP,  ____,                          KC_VOLD, ____,    ____,    ____,  ____,      \
         ____,  ____,                                                            ____,    ____,              \
                                    ____, ____,  ____, ____,                                                 \
                                    ____, ____,  ____, ____,                                                 \
@@ -99,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------|                             |------+------+------+------+------|
  * |      | !    | @    | #    | ~    |                             | 0    |  1   |  2   |  3   |      |
  * |------+------+------+-------------,                             ,-------------+------+------+------,
- *        |      | -    |                                                         |      |      |
+ *        | ~    | -    |                                                         |      |      |
  *        '------+------'-------------'                             '-------------'------+------'
  *                      |      |      |                             |      |      |
  *                      |      |      |                             |      |      |
