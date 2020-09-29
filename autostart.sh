d32a9fef559d5689db67cc2e7e627e4d7ab497fb
Oscar Morris <twocap06@gmail.com>
2020-09-29 07:40:25 +0100

    Add files via upload

---
+++ autostart.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#sshfs pi@192.168.1.124:/home/pi /home/oscar/RPI -C
+picom --experimental-backends --backend glx &
+feh --bg-fill ~/wallpapers/0051.jpg &
+flameshot &
+dunst &
+libinput-gestures &
+/home/oscar/scripts/github-sync.sh &
+brightnessctl s 50%
+syndaemon -i 1 &
+emacs --daemon &
+#onboard &
